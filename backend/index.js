9d9b9363c39618534753afd6ae2894672b76daf6
Carlos <chernandezweb@gmail.com>
2019-05-17 18:46:16 -0400

    test post build

--- backend/index.js
+++ backend/index.js
@@ -27,27 +27,27 @@ const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Backend listening on port ${port}!`));
 
 
-//
-// app.use(bodyParser.json());
-//
-// // Enable CORS from client-side
-// app.use(function(req, res, next) {
-//     res.header("Access-Control-Allow-Origin", "*");
-//     res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
-//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, Access-Control-Allow-Credentials, token");
-//     res.header("Access-Control-Allow-Credentials", "true");
-//     next();
-// });
-//
-// router(app);
-//
-// app.use(function(err, req, res, next) {
-//     console.log(err);
-//
-//     res.status(500).json({
-//         error:err
-//     });
-//
-// });
-//
-// module.exports = app;
+
+app.use(bodyParser.json());
+
+// Enable CORS from client-side
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, Access-Control-Allow-Credentials, token");
+    res.header("Access-Control-Allow-Credentials", "true");
+    next();
+});
+
+router(app);
+
+app.use(function(err, req, res, next) {
+    console.log(err);
+
+    res.status(500).json({
+        error:err
+    });
+
+});
+
+module.exports = app;
