d1fdc7897643cd81d84fa7e1919bef579a4f674f
Carlos <chernandezweb@gmail.com>
2019-05-17 17:55:01 -0400

    test post build

--- backend/index.js
+++ backend/index.js
@@ -5,23 +5,26 @@ const router = require('./router');
 
 const app = express();
 
-const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`Backend listening on port ${port}!`));
+// if(process.env.NODE_ENV === 'production') {
+//     app.use(express.static(path.join(__dirname, '../frontend/build')));
+//     //
+//     app.get('*', (req, res) => {
+//         res.sendfile(path.join(__dirname = '../frontend/build/index.html'));
+//     })
+// }
 
-if(process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../frontend/build')));
-    //
-    app.get('*', (req, res) => {
-        res.sendfile(path.join(__dirname = '../frontend/build/index.html'));
-    })
-}
+app.use(express.static(path.join(__dirname, '../frontend/build')));
 
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname+'../frontend/build/index.html'));
 });
+
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Backend listening on port ${port}!`));
 
 
 //
