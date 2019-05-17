22e002e2c7e64a12d8ca0da5e5e94fd67e8b4204
Carlos <chernandezweb@gmail.com>
2019-05-17 19:19:45 -0400

    test post build

--- backend/index.js
+++ backend/index.js
@@ -26,13 +26,13 @@ app.use(express.static(path.join(__dirname, '../frontend/build')));
 app.use(bodyParser.json());
 
 // Enable CORS from client-side
-// app.use(function(req, res, next) {
-//     res.header("Access-Control-Allow-Origin", "*");
-//     res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
-//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, Access-Control-Allow-Credentials, token");
-//     res.header("Access-Control-Allow-Credentials", "true");
-//     next();
-// });
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, Access-Control-Allow-Credentials, token");
+    res.header("Access-Control-Allow-Credentials", "true");
+    next();
+});
 
 router(app);
 
