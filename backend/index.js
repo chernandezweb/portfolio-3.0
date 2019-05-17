c534c770b34b723708bde3e4b77188446458f3bb
Carlos <chernandezweb@gmail.com>
2019-05-17 16:31:34 -0400

    test post build

--- backend/index.js
+++ backend/index.js
@@ -9,6 +9,14 @@ app.listen(port, () => console.log(`Backend listening on port ${port}!`));
 
 app.use(bodyParser.json());
 
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../frontend/build')));
+    //
+    app.get('*', (req, res) => {
+        res.sendfile(path.join(__dirname = '../frontend/build/index.html'));
+    })
+}
+
 // Enable CORS from client-side
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
