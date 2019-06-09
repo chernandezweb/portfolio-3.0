9309f4a8f43302058ff5f77bbd129d092413b45a
Carlos <chernandezweb@gmail.com>
2019-06-09 18:54:12 -0400

    remove useless commented code in backend

--- backend/index.js
+++ backend/index.js
@@ -9,19 +9,7 @@ const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Backend listening on port ${port}!`));
 
-
-// if(process.env.NODE_ENV === 'production') {
-//     app.use(express.static(path.join(__dirname, '../frontend/build')));
-//     //
-//     app.get('*', (req, res) => {
-//         res.sendfile(path.join(__dirname = '../frontend/build/index.html'));
-//     })
-// }
-
 app.use(express.static(path.join(__dirname, '../frontend/build')));
-
-
-
 
 app.use(bodyParser.json());
 
--- backend/router.js
+++ backend/router.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const ProjectsController = require('./controllers/projects.js');
-      // AuthentificationController = require('./controllers/authentification');
 
 module.exports = function(app) {
     const apiRoutes = express.Router();
